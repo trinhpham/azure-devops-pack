213387fb8dbf4fa0309363e75428bb3efef88278
Trinh Pham <trinh.pham@logigear.com>
2018-04-27 18:01:59 +0700

    Add task

--- buildAndReleaseTask/DownloadFTPArtifact/DownloadFTPArtifact.ts
+++ buildAndReleaseTask/DownloadFTPArtifact/DownloadFTPArtifact.ts
@@ -1,4 +1,28 @@
 import tl = require("vsts-task-lib/task");
 import tr = require("vsts-task-lib/toolrunner");
 
-tl.command()
+import {TaskQueue} from 'typescript-task-queue'
+
+let taskQueue = new TaskQueue();
+
+// Add a function to the queue
+taskQueue.enqueue(() => {
+    // Some async code will run here
+    // ...
+});
+
+// Start the queue
+taskQueue.start();
+
+// Stop the queue
+taskQueue.
+
+// Callback on start
+taskQueue.on('start', () => {
+    // ...
+});
+
+// Callback on stop
+taskQueue.on('stop', () => {
+    // ...
+});

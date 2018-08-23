3b686fb77bf222b8437d54859690d5bf47c0d4a9
Trinh Pham <trinh.pham@logigear.com>
2018-08-23 18:40:26 +0700

    impl

--- buildAndReleaseTask/PublishTestPlanResults/PublishTestPlanResults.ts
+++ buildAndReleaseTask/PublishTestPlanResults/PublishTestPlanResults.ts
@@ -3,20 +3,23 @@
 import tl = require("vsts-task-lib/task");
 import url = require("url");
 import fs = require("fs");
-import shell = require("shelljs");
 import Q = require('q');
-
-
-const ftpServer = "ftp-server";
-const endpointUsername = "username";
-const endpointPassword = "password";
-const ftpDir = "ftp-directory";
-const targetDir = "target-directory";
+import process = require('process');
 
 async function run() {
     try {
+        var resultFile, testConfiguration, testSuite, testPlan;
+
+        if (process.argv.includes("hlv-debug")){
+            tl.debug("Enter debug mode");
+        } else {
+            resultFile = tl.getInput("resultFile");
+            testConfiguration = tl.getInput("testConfiguration");
+            testSuite = tl.getInput("testSuite");
+            testPlan = tl.getInput("testPlan");
+        }
         tl.debug("Enter PublishTestPlanResults");
-        
+        tl.debug("")
     } catch (err) {
         tl.setResult(tl.TaskResult.Failed, 'PublishTestPlanResults FAILED ' + err.message);
     }

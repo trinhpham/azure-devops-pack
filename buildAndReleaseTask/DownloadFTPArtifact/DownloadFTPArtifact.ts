48b7da11408fa1eb462c73349ea522eff4d9f47f
Trinh Pham <trinh.pham@logigear.com>
2019-04-12 16:05:35 +0700

    - Update libraries - Improve TS code (enable tslint, fix un-safe check) - Debuggable

--- buildAndReleaseTask/DownloadFTPArtifact/DownloadFTPArtifact.ts
+++ buildAndReleaseTask/DownloadFTPArtifact/DownloadFTPArtifact.ts
@@ -1,33 +1,34 @@
-'use strict';
-import tl = require("vsts-task-lib/task");
-import Client = require('ftp');
-import url = require("url");
-import fs = require("fs");
-import shell = require("shelljs");
-import Q = require('q');
+"use strict";
 
-const ftpServer = "ftp-server";
+import fs = require("fs");
+import Client = require("ftp");
+import path = require("path");
+import Q = require("q");
+import shell = require("shelljs");
+import url = require("url");
+import tl = require("vsts-task-lib/task");
+
+const ftpServerConst = "ftp-server";
 const endpointUsername = "username";
 const endpointPassword = "password";
-const ftpDir = "ftp-directory";
-const targetDir = "target-directory";
+const ftpDirConst = "ftp-directory";
+const targetDirConst = "target-directory";
 
-
-let ftpConfig = null;
+let ftpConfig: Client.Options = null;
 
 async function downloadFile(itemPath, item: Client.ListingElement, targetDir: string): Promise<void> {
-    let fileDownloadDefer: Q.Deferred<void> = Q.defer<void>();
-    console.log("=============Start get: " + itemPath);
-    let fileFTP = new Client();
+    const fileDownloadDefer: Q.Deferred<void> = Q.defer<void>();
+    console.log(`=============Start get: ${itemPath}`);
+    const fileFTP = new Client();
     fileFTP.on("ready", () => {
         fileFTP.get(itemPath, (error, stream: NodeJS.ReadableStream) => {
             try {
                 if (error) {
-                    console.error("Unable to download '%s', error: %s", itemPath, error.message);
-                    tl.setResult(tl.TaskResult.Failed, error.message);
+                    console.error(`Unable to download '${itemPath}', error: ${error.message}`);
+                    failTask(`Unable to download '${itemPath}', error: ${error.message}`);
                 } else {
-                    stream.pipe(fs.createWriteStream(targetDir + '/' + item.name));
-                    console.log("Wrote file: " + targetDir + '/' + item.name);
+                    stream.pipe(fs.createWriteStream(path.join(targetDir, item.name)));
+                    console.log("Wrote file: " + path.join(targetDir, item.name));
                 }
             } catch (e) {
                 console.log("Error when writing file: " + itemPath);
@@ -44,36 +45,35 @@ async function downloadFile(itemPath, item: Client.ListingElement, targetDir: st
 }
 
 async function downloadFromFTP(ftp: Client, ftpDir: string, targetDir: string): Promise<void> {
-    let defer: Q.Deferred<void> = Q.defer<void>();
+    const defer: Q.Deferred<void> = Q.defer<void>();
     shell.mkdir("-p", targetDir);
     ftp.list(
         ftpDir,
         async (_, listing) => {
             try {
                 if (listing) {
-                    for (let i = 0; i < listing.length; i++) {
-                        let item = listing[i];
+                    listing.forEach(async (item) => {
                         try {
-                            let itemPath = ftpDir + '/' + item.name;
+                            const itemPath = ftpDir + "/" + item.name;
                             tl.debug("Processing: " + itemPath);
-                            if (item.type == 'd') {
-                                shell.mkdir("-p", targetDir + '/' + item.name);
-                                await downloadFromFTP(ftp, itemPath, targetDir + '/' + item.name);
-                            } else if (item.type == '-') {
+                            if (item.type === "d") {
+                                shell.mkdir("-p", path.join(targetDir, item.name));
+                                await downloadFromFTP(ftp, itemPath, path.join(targetDir, item.name));
+                            } else if (item.type === "-") {
                                 await downloadFile(itemPath, item, targetDir);
                             } else {
-                                console.log("Unsupported FTP file type '%s' for: %s", item.type, itemPath);
+                                console.log(`Unsupported FTP file type '${item.type}' for: ${itemPath}`);
                             }
                         } catch (err) {
-                            tl.setResult(tl.TaskResult.Failed, "Error happens: " + err.message);
+                            failTask("Error happens: " + err.message);
                             return;
                         }
-                    }
+                    });
                 }
             } finally {
                 defer.resolve(null);
             }
-        }
+        },
     );
     return defer.promise;
 }
@@ -82,49 +82,57 @@ async function run() {
     try {
         tl.debug("Enter downloadFromFTP FTP Artifact");
         let host, port, user, pass, inDir, outDir: string, isSecured: boolean;
-        if (tl.getInput("HLV_DEV") != null) {
-            //For debug only
+        if (process.argv.includes("HLV_DEV")) {
+            // For debug only
+            console.log("Entering debug mode");
+            const debugInfo = require("./debug.json");
+            ({host, port, user, pass, inDir, outDir, isSecured} = debugInfo);
         } else {
-            let endpointName = tl.getInput(ftpServer);
+            const endpointName = tl.getInput(ftpServerConst);
             tl.debug("endpointName: " + endpointName);
-            let endpointUrl = tl.getEndpointUrl(endpointName, false);
+            const endpointUrl = tl.getEndpointUrl(endpointName, false);
             tl.debug("url: " + endpointUrl);
-            if (endpointUrl == null) {
-                tl.setResult(tl.TaskResult.Failed, "Invalid FTP Endpoint URL: " + endpointUrl);
+            if (!endpointUrl) {
+                failTask("Invalid FTP Endpoint URL: " + endpointUrl);
                 return;
             }
-            let parsedUrl = url.parse(endpointUrl);
+            const parsedUrl = url.parse(endpointUrl);
             host = parsedUrl.host;
             port = parsedUrl.port;
-            if (port == null || port == "" || port <= 0) {
+            if (!port || port <= 0) {
                 port = "21";
             }
 
-            isSecured = parsedUrl.protocol == "ftps";
+            isSecured = parsedUrl.protocol === "ftps";
             user = tl.getEndpointAuthorizationParameter(endpointName, endpointUsername, false);
             pass = tl.getEndpointAuthorizationParameter(endpointName, endpointPassword, false);
-            outDir = tl.getInput(targetDir);
-            if (outDir != null && !outDir.endsWith("/")) {
-                outDir += "/";
+            outDir = tl.getInput(targetDirConst);
+            if (!outDir) {
+                failTask(`Invalid output directory: ${outDir}`);
+                return;
             }
 
-            inDir = tl.getInput(ftpDir);
-            if (inDir != null && inDir.endsWith("/")) {
+            inDir = tl.getInput(ftpDirConst);
+            if (!inDir) {
+                failTask(`Invalid input directory: ${inDir}`);
+                return;
+            }
+            if (inDir.endsWith("/")) { // trim ending slash
                 inDir = inDir.substring(0, inDir.lastIndexOf("/"));
             }
         }
 
-        let ftpClient: any = new Client();
+        const ftpClient = new Client();
 
         let downloadSuccessful: boolean = false;
 
-        ftpClient.on('greeting', (message: string) => {
-            tl.debug('ftp client greeting');
-            console.log('FTPConnected: ' + message);
+        ftpClient.on("greeting", (message: string) => {
+            tl.debug("ftp client greeting");
+            console.log("FTPConnected: " + message);
         });
 
-        ftpClient.on('ready', async () => {
-            tl.debug('ftp client ready');
+        ftpClient.on("ready", async () => {
+            tl.debug("ftp client ready");
             try {
                 await downloadFromFTP(ftpClient, inDir, outDir);
                 downloadSuccessful = true;
@@ -132,57 +140,47 @@ async function run() {
             } catch (err) {
                 failTask(err);
             } finally {
-                console.log('DisconnectHost');
+                console.log("DisconnectHost");
                 ftpClient.end();
                 ftpClient.destroy();
             }
         });
 
-        ftpClient.on('close', (hadErr: boolean) => {
-            console.log('Disconnected');
-            tl.debug('ftp client close, hadErr:' + hadErr);
+        ftpClient.on("close", (hadErr: boolean) => {
+            console.log("Disconnected");
+            tl.debug("ftp client close, hadErr:" + hadErr);
         });
 
-        ftpClient.on('end', () => {
-            tl.debug('ftp client end');
+        ftpClient.on("end", () => {
+            tl.debug("ftp client end");
         });
 
-        ftpClient.on('error', (err) => {
-            tl.debug('ftp client error, err: ' + err);
+        ftpClient.on("error", (err) => {
+            tl.debug("ftp client error, err: " + err);
             if (!downloadSuccessful) {
                 // once all files are successfully downloaded, a subsequent error should not fail the task
                 failTask(err);
             }
         });
 
-        let verboseSnippet: string[] = [];
-        let debugLogger: any = function (message) {
-            verboseSnippet.push(message);
-            if (verboseSnippet.length >= 5) {
-                verboseSnippet.shift();
-            }
-            tl.debug(message);
-        };
-
-        function failTask(message: string): void {
-            let fullMessage: string = `FTP download failed: "${message}". FTP log: "${verboseSnippet}".`;
-            console.log(fullMessage);
-            tl.setResult(tl.TaskResult.Failed, message);
-        }
-
         ftpConfig = {
-            'host': host,
-            'port': +port,
-            'user': user,
-            'password': pass,
-            'secure': isSecured,
-            'debug': debugLogger
+            host,
+            port: +port,
+            user,
+            password: pass,
+            secure: isSecured,
         };
         ftpClient.connect(ftpConfig);
         return;
     } catch (err) {
-        tl.setResult(tl.TaskResult.Failed, 'DownloadFTPArtifactFAILED: ' + err.message);
+        failTask("DownloadFTPArtifactFAILED: " + err.message);
     }
+}
+
+function failTask(message: string): void {
+    const fullMessage: string = `FTP download failed: "${message}".`;
+    console.log(fullMessage);
+    tl.setResult(tl.TaskResult.Failed, message);
 }
 
 run();
